414a9d6314878db13f8a48b3b9198598f278f2f4
Edward Tanguay (windows) <edward@tanguay.info>
2025-07-23 18:06:08 +0200

    CONTENT: add 27

--- scripts/cmd/create-prompt-files/main.go
+++ scripts/cmd/create-prompt-files/main.go
@@ -150,7 +150,7 @@ func main() {
 		})
 
 		// create prompt files
-		numberOfPhrases := 50 // nnn
+		numberOfPhrases := 52 // nnn
 		numberOfWords := 550
 		// fmt.Println("number of flashcards: ", len(flashcardsByLanguage[language]))
 		if mode == "random" {
