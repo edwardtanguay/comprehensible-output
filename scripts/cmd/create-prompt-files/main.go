71de5a60c1a5d405311395d72d05e3d4f8be1351
Edward Tanguay (windows) <edward@tanguay.info>
2025-08-01 11:44:06 +0200

    CONTENT: ital audio aug 4

--- scripts/cmd/create-prompt-files/main.go
+++ scripts/cmd/create-prompt-files/main.go
@@ -150,8 +150,8 @@ func main() {
 		})
 
 		// create prompt files
-		numberOfPhrases := 51 // nnn
-		numberOfWords := 550
+		numberOfPhrases := 68 // nnn
+		numberOfWords := 650
 		// fmt.Println("number of flashcards: ", len(flashcardsByLanguage[language]))
 		if mode == "random" {
 			flashcardsByLanguage[language] = Randomize(flashcardsByLanguage[language])
